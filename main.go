0ae5ecf258c26ca9bf28f8e7d3f5a28196f31610
Aashish Rathi <aashishrathi2013@gmail.com>
2020-04-05 17:54:11 +0530

    Static HTML page served

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+		
+		`))
+	})
+	//start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
